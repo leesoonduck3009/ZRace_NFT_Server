16f0e848daf6da4ebea845a09fd71cc492e4053d
PNB309 <nguyen.phucbinh445@gmail.com>
2024-04-30 00:23:29 +0700

    Update wallet balances

--- controller/walletController.js
+++ controller/walletController.js
@@ -1,13 +1,18 @@
 const Moralis = require ('moralis').default;
 const asyncHandler = require('express-async-handler')
-
+require('dotenv').config();
+const CHAIN = process.env.CHAIN;
+const COIN_CONTRACT_ADDRESS = process.env.COIN_CONTRACT_ADDRESS;
 const getWalletBalance = async (req,res)  => {
     try{
 
-        const response = await Moralis.EvmApi.balance.getNativeBalance({
-        "chain": "0x61",
-        "address": req.body.address
-        });
+        const response = await Moralis.EvmApi.token.getWalletTokenBalances({
+            "chain": "0x61",
+            "tokenAddresses": [
+                COIN_CONTRACT_ADDRESS
+            ],
+            "address": req.body.address
+          });
         console.log(response)
         const dataRespone = {
             address: req.body.address,
